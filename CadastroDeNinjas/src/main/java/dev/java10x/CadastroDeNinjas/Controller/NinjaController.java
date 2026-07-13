51170a49dacfe0af3ec5baa75ecbe36f71c90ea7
Alysson Manoel <alysson.manoelsv@gmail.com>
2026-07-13 14:00:03 -0300

    criando o Controller e o Model

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Controller/NinjaController.java
@@ -0,0 +1,11 @@
+package dev.java10x.CadastroDeNinjas.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastro")
+public class NinjaController {
+
+
+}
---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Model/Ninja.java
@@ -0,0 +1,26 @@
+package dev.java10x.CadastroDeNinjas.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ninja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer idade;
+
+}
